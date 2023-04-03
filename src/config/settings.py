f51e05134bcfdb2dadbc87983f524998b473a101
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-04-04 01:28:09 +0600

    added initial user class

---
+++ src/config/settings.py
@@ -0,0 +1,3 @@
+baseurl = 'https://moodle.cse.buet.ac.bd/'
+id = '1705003'
+pwd = 'haha you will never guess it'
---
+++ src/user/user.py
@@ -0,0 +1,29 @@
+# import required module
+import sys
+ 
+# append the path of the
+# parent directory
+sys.path.append("..")
+
+from config.settings import baseurl
+from requests import session
+
+class User:
+	
+	def __init__(self,user,pwd) -> None:
+		authdata = {
+			'action': 'login',
+			'username': user,
+			'password': pwd
+		}
+		self.session = session()
+		self.session.post(baseurl + 'login/index.php', data=authdata)
+		assert self.session.get(baseurl + 'my/').status_code == 200
+	
+	def get(self, url):
+		return self.session.get(baseurl + url)
+	
+	def get_absolute(self, url):
+		return self.session.get(url)
+
+	
