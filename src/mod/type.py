55e6ef0ceb00f6bd8decdc50c315787ecc6ec3d2
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-04-04 20:53:55 +0600

    updated types

--- src/mod/type.py
+++ src/mod/type.py
@@ -5,15 +5,19 @@ from config.settings import baseurl
 
 class Type(Enum):
 	ASSIGN = 1
-	FORUM_VIEW = 2
-	FORUM_DISCUS = 3
-	RESOURCE = 4
-	URL = 5
-	OUT = 6
 	FOLDER = 7
 	PAGE = 8
-	FILE = 9
 	DATA = 10
+	FILE = 9
+	FORUM_VIEW = 2
+	FORUM_DISCUS = 3
+	FORUM_OTHER = 4
+	RESOURCE = 5
+	URL = 6
+	COURSE_VIEW = 11
+	USER_VIEW = 12
+	OTHER = 13
+
 
 def get_type(url):
 	"""
@@ -25,6 +29,8 @@ def get_type(url):
 		return Type.FORUM_VIEW
 	elif baseurl+'mod/forum/discuss.php' in url:
 		return Type.FORUM_DISCUS
+	elif baseurl+'mod/forum/' in url:
+		return Type.FORUM_OTHER
 	elif baseurl+'mod/resource/' in url:
 		return Type.RESOURCE
 	elif baseurl+'mod/url/' in url:
@@ -37,8 +43,12 @@ def get_type(url):
 		return Type.FILE
 	elif baseurl+'mod/data/' in url:
 		return Type.DATA
+	elif baseurl+'course/view.php' in url:
+		return Type.COURSE_VIEW
+	elif baseurl+'user/view.php' in url:
+		return Type.USER_VIEW
 	elif baseurl in url:
 		assert False, f'unknown url {url}'
 	else:
 		print("url is out of moodle: ", url)
-		return Type.OUT
+		return Type.OTHER
