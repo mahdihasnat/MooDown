05aa66d8554d4ddfae934a67d2fd7c6233cb1b94
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-04-05 01:48:01 +0600

    removed url encoding problem

--- src/utils/url.py
+++ src/utils/url.py
@@ -1,6 +1,4 @@
-from hyperlink import parse
-
+from urllib.parse import quote
 
 def encode_url(url_text):
-	url_text = url_text.replace(':', '%3A')
-	return parse(url_text,decoded = False).to_text()
+	return quote(url_text)
